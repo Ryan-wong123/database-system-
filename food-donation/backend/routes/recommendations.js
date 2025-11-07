8a611e5980d8901b00da909a394268ff16e68cd9
ryan wong <42606643+Ryan-wong123@users.noreply.github.com>
2025-11-07 23:52:53 +0800

    remove unnecessary code for diet reccomendations

--- food-donation/backend/routes/recommendations.js
+++ food-donation/backend/routes/recommendations.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { recommendItemsForHousehold } = require("../services/recommendations");
+const { recommendItemsForHousehold } = require("../db/booking");
 
 // +++ add these lines +++
 const { connectMongo } = require("../db/index");
